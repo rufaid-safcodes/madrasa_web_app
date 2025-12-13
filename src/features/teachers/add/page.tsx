4368b8ab6fbc51d8a574a5f9f3a8b600e42d001f
muhammedrufaid <mohdrufaid7@gmail.com>
2025-12-13 13:30:23 +0400

    formatted

--- src/features/teachers/add/page.tsx
+++ src/features/teachers/add/page.tsx
@@ -1,8 +1,10 @@
 // src/features/teachers/add/page.tsx
-import { TeacherForm } from '../TeacherForm';
+import { TeacherForm } from "../TeacherForm";
 
 export default function AddTeacher() {
-  return <div className='flex items-center justify-center w-full'>
-    <TeacherForm mode="ADD" />
-  </div>
+  return (
+    <div className="flex items-center justify-center w-full">
+      <TeacherForm mode="ADD" />
+    </div>
+  );
 }
